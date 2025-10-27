b53d05abafeba8ff0c3c3a8c4f96721548fe419f
David Sliren <bluee.97.berry@gmail.com>
2025-10-27 18:42:21 -0500

    style: Retocar la plantilla de contactos y las skills

--- src/components/001 Plantillas/Contacts.jsx
+++ src/components/001 Plantillas/Contacts.jsx
@@ -5,15 +5,24 @@ function Contacts() {
   return (
     <>
       <article className="flex gap-2 mt-2">
-        <Icons1 className="bg-zinc-900 hover:bg-zinc-600">
+        <Icons1
+          className="bg-zinc-900 hover:bg-zinc-600"
+          link="https://github.com/David-Sliren"
+        >
           <Github />
         </Icons1>
 
-        <Icons1 className="bg-sky-400 hover:bg-sky-600">
+        <Icons1
+          className="bg-sky-400 hover:bg-sky-600"
+          link="https://www.linkedin.com/in/davidsliren/"
+        >
           <Linkedin />
         </Icons1>
 
-        <Icons1 className="bg-green-600 hover:bg-green-400">
+        <Icons1
+          className="bg-green-600 hover:bg-green-400"
+          link="mailto:jhojansalazar79@gmail.com?subject=Me%20encuentro%20interesado%20en%20contratarte&body=Escribeme.."
+        >
           <Mail />
         </Icons1>
 

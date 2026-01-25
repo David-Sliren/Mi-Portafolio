de4d4ba68a6982cb608651956e8c6af30ee557ff
David Sliren <bluee.97.berry@gmail.com>
2026-01-24 23:22:19 -0500

    style(home): Mejorar rendimiento y estructura

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,24 +6,27 @@ import Certificates from "../components/Certificates/Certificates";
 import Icons1 from "../components/001 Plantillas/Icons1";
 
 import { Mail } from "lucide-react";
+import { Phone } from "lucide-react";
 
 function Home() {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] w-full md:w-[710px] min-h-full">
-      <header className="flex flex-col gap-2 items-center border-white border-b-2 pb-6">
-        <div className="w-full flex gap-4 items-center py-6 px-4 ">
-          <Presentation />
-        </div>
-        <div className="w-full">
-          <AboutMe />
-        </div>
-      </header>
-      <main className="flex flex-col gap-15 bg-transparent overflow-x-hidden py-5 pb-10">
-        <Skills />
-        <Proyects />
-        <Certificates />
-      </main>
-      <footer className="flex flex-col gap-2 items-center bg-zinc-900 p-4 py-6">
+    <div className="container">
+      <div className="grid grid-rows-[auto_1fr] w-full md:w-[710px] min-h-full mx-auto">
+        <header className="flex flex-col gap-2 items-center border-white border-b-2 pb-6">
+          <div className="w-full flex gap-4 items-center py-6 px-4 ">
+            <Presentation />
+          </div>
+          <div className="w-full">
+            <AboutMe />
+          </div>
+        </header>
+        <main className="flex flex-col gap-15 bg-transparent overflow-x-hidden py-5 pb-10">
+          <Skills />
+          <Proyects />
+          <Certificates />
+        </main>
+      </div>
+      <footer className="flex flex-col gap-2 items-center bg-zinc-900 p-4 py-6 min-w-full">
         <h2 className="text-2xl text-sky-500 font-bold">Contactame:</h2>
         <Icons1
           className="bg-sky-600 hover:bg-green-400 gap-2"
@@ -34,6 +37,7 @@ function Home() {
           <Mail />
           <span>Email</span>
         </Icons1>
+
         <div className="w-full text-ms text-zinc-500 flex flex-col items-center mt-4">
           <p className="">© 2025 David Sliren.</p>
           Casi todos los derechos reservados

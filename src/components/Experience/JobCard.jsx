2dd753e6a0d469ebb7f638fc00d306d6b608d8fa
David Sliren <bluee.97.berry@gmail.com>
2026-05-21 00:25:13 -0500

    style(components/Experience/JobCard): Retocar texto de tarjeta, y borrar cotenedor de tecnologias

--- src/components/Experience/JobCard.jsx
+++ src/components/Experience/JobCard.jsx
@@ -8,25 +8,29 @@ const JobCard = () => {
           <div className="flex items-center gap-3 mb-2">
             <LuTerminal color="#abc7ff" className="hidden sm:block" />
             <h3 className="text-2xl font-bold">
-              Desarrollador FullStack Multiplataforma
+              Desarrollador FullStack Javascript
             </h3>
           </div>
           <p className="text-md sm:text-lg text-white/60 font-medium mb-4 flex items-center gap-x-2 ">
             <LuBriefcaseBusiness size={18} className="hidden sm:block" />
-            Freelancer independiente / Remoto
+            Freelancer profecional independiente / Remoto
           </p>
           <div className="space-y-4 mb-6">
             <p className="text-neutral-500 text-sm font-medium leading-relaxed">
-              Me especialice en la creación de interfaces web responsivas y
-              servicios backend escalables. Desarrollando proyectos reales
-              enfocados en productividad y experiencia de usuario, aplicando
-              buenas prácticas de arquitectura, modularidad y rendimiento.
+              Desarrollo de aplicaciones web y móviles enfocadas en arquitectura
+              escalable, modularidad y experiencia de usuario.
+              <br />
+              <br />
+              Responsabilidades y enfoque técnico:
+              <br />
+              <br />
+              - Construcción de SPAs utilizando React y React Router.
+              <br />
+              - Desarrollo de aplicaciones móviles con React Native y Expo.
+              <br />- Diseño de arquitecturas frontend modulares utilizando
+              Zustand (slices).
+              <br />- Desarrollo de REST APIs con Node.js, Express y MongoDB.
             </p>
-            <div className="flex flex-wrap gap-2">
-              <span className="text-[10px] font-bold uppercase tracking-widest bg-shark-700 px-3 py-1 rounded-full text-blue-300/80 ">
-                React.js
-              </span>
-            </div>
           </div>
           <div className="bg-shark-900/80 p-4 rounded-xl">
             <p className="text-xs font-bold uppercase tracking-widest text-blue-300 mb-2">
@@ -44,8 +48,8 @@ const JobCard = () => {
                 <span className="material-symbols-outlined text-xs mt-1 mr-2 text-primary">
                   <LuCircleCheck size={10} className="text-blue-300" />
                 </span>{" "}
-                Domino la lógica del lado del servidor y la persistencia de
-                bases de datos mediante Mongo y SQL.
+                Domine la lógica del lado del servidor y la persistencia de
+                bases de datos mediante MongoDB.
               </li>
             </ul>
           </div>

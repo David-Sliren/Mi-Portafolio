deb0e5be51bc84ea4435956b0d609c23b201c8ea
David Sliren <bluee.97.berry@gmail.com>
2026-05-29 21:20:49 -0500

    feact(Contanst): Actualizar Iconos de tecnologias

--- src/constants/constantsIcons.js
+++ src/constants/constantsIcons.js
@@ -4,18 +4,21 @@ export const SkillsIcons = [
   { iconName: "SiHtml5", label: "HTML" },
   { iconName: "SiCss", label: "CSS" },
   { iconName: "SiJavascript", label: "JavaScrpit" },
+  { iconName: "SiTypescript", label: "TypeScrpit" },
   { iconName: "SiTailwindcss", label: "Tailwindcss" },
   { iconName: "SiNextdotjs", label: "Next js" },
   { iconName: "SiReact", label: "React" },
   { iconName: "SiReact", label: "React-Native" },
   { iconName: "SiReactrouter", label: "React-Router" },
   { iconName: "SiReactquery", label: "TanStack-Query" },
+  { iconName: "SiRedux", label: "Redux" },
   { iconName: "TbBrandFramerMotion", label: "Motion" },
-  { iconName: "SiFirebase", label: "Firebase" },
   { iconName: "SiExpo", label: "Expo" },
+  { iconName: "SiFirebase", label: "Firebase" },
   { iconName: "SiNodedotjs", label: "Node js" },
   { iconName: "SiExpress", label: "Express" },
   { iconName: "SiMongodb", label: "MongoDB" },
+  { iconName: "SiMongoose", label: "Mongoose" },
 ];
 
 SkillsIcons.forEach((item) => SkillHash.set(item.label, item.iconName));

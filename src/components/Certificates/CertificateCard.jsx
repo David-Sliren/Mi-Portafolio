8951db3c0f1a9ccb4cd54f5859efe0cf928b2ced
David Sliren <bluee.97.berry@gmail.com>
2026-05-30 21:15:57 -0500

    style(comonents/Cetificates): Colocar etiqueta 'a', a el componente principal, cambiar nombre a boton 'ver proyecto', por 'ver certificado'.

--- src/components/Certificates/CertificateCard.jsx
+++ src/components/Certificates/CertificateCard.jsx
@@ -3,7 +3,11 @@ import { LuCircleArrowOutUpRight } from "react-icons/lu";
 
 const CertificateCard = ({ title, description, place, date, imgsrc, link }) => {
   return (
-    <div className="col-span-full sm:col-span-3 lg:col-span-4 group bg-gradient-blue rounded-xl p-6 border border-outline-variant/10 transition-all duration-300 hover:scale-105 brightness-85 timeline-view animate-range-early-entry animate-fade-in-right overflow-hidden">
+    <a
+      href={link}
+      target="_blank"
+      className="col-span-full sm:col-span-3 lg:col-span-4 group bg-gradient-blue rounded-xl p-6 border border-outline-variant/10 transition-all duration-300 hover:scale-105 brightness-85 timeline-view animate-range-early-entry animate-fade-in-right overflow-hidden"
+    >
       <div className="w-full h-20 rounded-lg m-auto mb-6 flex items-center justify-center overflow-hidden">
         <img
           className="size-full object-cover object-top"
@@ -27,18 +31,14 @@ const CertificateCard = ({ title, description, place, date, imgsrc, link }) => {
         </div>
       </div>
       <div className="hidden absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 sm:flex items-center justify-center z-10 backdrop-blur-xl">
-        <a
-          href={link}
-          target="_blank"
-          className="bg-white/5 px-8 py-4 rounded-full font-headline font-bold text-white flex items-center space-x-2 border border-white/20 backdrop-blur-3xl cursor-pointer z-10 hover:bg-white/10 hover:animate-jelly"
-        >
-          <span>Ver proyecto</span>
+        <button className="bg-white/5 px-8 py-4 rounded-full font-headline font-bold text-white flex items-center space-x-2 border border-white/20 backdrop-blur-3xl cursor-pointer z-10 hover:bg-white/10 hover:animate-jelly">
+          <span>Ver certificado</span>
           <span className=" text-sm">
             <LuCircleArrowOutUpRight />
           </span>
-        </a>
+        </button>
       </div>
-    </div>
+    </a>
   );
 };
 

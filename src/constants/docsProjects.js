ce2693ed271f5c801d9381ed7f6003168d3718cb
David Sliren <bluee.97.berry@gmail.com>
2026-07-29 20:15:50 -0500

    refactor: Agregar proyecto de landing page de chocolate andaluz, cambiar descripcion de los proyectos

--- src/constants/docsProjects.js
+++ src/constants/docsProjects.js
@@ -3,52 +3,62 @@ Procure escribir los proyectos mas actuales arriba ya que la utilidad usada para
 */
 export const projectsData = [
   {
-    title: "A | Contribucion",
+    title: "Aleida | Contribucion",
     descripcion:
-      "Este proyecto surgio por la necesidad de saber cuanto dinero se habia aportado para ayudar a mi abuela con su enfermedad y cual era el estado de sus medicinas y suplementos.",
-
+      "Aplicación web que permite a una comunidad o grupo familiar financiar colectivamente los gastos de un paciente, monitorear en tiempo real sus medicamentos y gestionar los pagos de sus cuidadores con roles de acceso transparentes.",
     platform: "Web",
     stack: ["Next js", "MongoDB", "Mongoose", "Tailwindcss", "Motion"],
     img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1778212368/Aleida-Contribution_xjbdbg.png",
     link: "https://aleida-contribucion.vercel.app/",
   },
-
-  {
-    title: "Tienda Sliren",
-    descripcion:
-      "El proyecto funciona como un portafolio interactivo y una tienda de aplicaciones, permitiendo a los usuarios explorar, conocer detalles y descargar las últimas versiones de las apps disponibles..",
-
-    platform: "Web",
-    stack: ["React", "Tailwindcss", "React-Router", "Motion"],
-    img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1775804567/2a18ffe2-0548-42f5-83a5-b89512446b00.png",
-    link: "https://sliren-store.vercel.app/",
-  },
   {
     title: "Renpentain",
     descripcion:
-      "Es una aplicacion movil diseñada para simplificar y organizar tus listas de la compra y tus visitas al supermercado.",
+      "Aplicacion movil diseñada para simplificar y organizar las listas de compras y las visitas al supermercado de forma eficiente.",
     platform: "Mobil",
     stack: ["React", "Tailwindcss", "TanStack-Query", "React-Router", "Motion"],
     img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1775803312/0f4a658a-804d-4294-8b3d-43d4839c7cd3.png",
     link: "https://renpentain.uptodown.com/android",
   },
   {
-    title: "Fútbol Mania",
+    title: "Chocolate Andalucia",
     descripcion:
-      "Este proyecto es una aplicación web completa para visualizar información de fútbol, incluyendo partidos en vivo, clasificaciones y competiciones. Está construido con una arquitectura moderna separando el frontend y el backend.",
+      "Landing page para aumentar las ventas de los cupos de una pasadia en la finca de la fabrica chocolate andaluz",
+
     platform: "Web",
-    stack: [
-      "React",
-      "Tailwindcss",
-      "TanStack-Query",
-      "React-Router",
-      "Motion",
-      "Node js",
-      "Express",
-    ],
-    img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1775924100/04FutbolMania_eit1mo.webp",
-    link: "https://david-sliren.github.io/Futbol-Mania/",
+    stack: ["React", "Tailwindcss", "Motion"],
+    img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1785363568/chocolate_andalucia_dwnvni.webp",
+    link: "https://chocolate-andaluz.vercel.app/",
   },
+
+  {
+    title: "Tienda Sliren",
+    descripcion:
+      "Portafolio interactivo y tienda de aplicaciones, que permite a los usuarios explorar, conocer detalles y descargar las últimas versiones de mis aplicaciones moviles",
+
+    platform: "Web",
+    stack: ["React", "Tailwindcss", "React-Router", "Motion"],
+    img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1775804567/2a18ffe2-0548-42f5-83a5-b89512446b00.png",
+    link: "https://sliren-store.vercel.app/",
+  },
+
+  // {
+  //   title: "Fútbol Mania",
+  //   descripcion:
+  //     "Este proyecto es una aplicación web completa para visualizar información de fútbol, incluyendo partidos en vivo, clasificaciones y competiciones. Está construido con una arquitectura moderna separando el frontend y el backend.",
+  //   platform: "Web",
+  //   stack: [
+  //     "React",
+  //     "Tailwindcss",
+  //     "TanStack-Query",
+  //     "React-Router",
+  //     "Motion",
+  //     "Node js",
+  //     "Express",
+  //   ],
+  //   img: "https://res.cloudinary.com/dudiot7uv/image/upload/v1775924100/04FutbolMania_eit1mo.webp",
+  //   link: "https://david-sliren.github.io/Futbol-Mania/",
+  // },
   // {
   //   title: "Movies new",
   //   descripcion:
